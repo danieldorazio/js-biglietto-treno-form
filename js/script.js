309625f7b64d4d679e26f29c86f95e14f80b923c
danieldorazio <danieldorazio.97@gmail.com>
2023-10-19 15:30:50 +0200

    add: js: LOGICA DEL PROGRAMMA

--- js/script.js
+++ js/script.js
@@ -9,6 +9,11 @@ console.log(distanza);
 const userEtaElement = document.getElementById ("eta");
 console.log(userEtaElement);
 
+const prezzoBase = 0.21;
+const scontoMinori = 0.20;
+const scontoOver = 0.40;
+
+
 
 /////////////FUNZIONAMENTO AL CLICK////////////
 const sendBtn = document.getElementById("send");
@@ -17,11 +22,29 @@ sendBtn.addEventListener("click", function () {
     //RACCOLTA DATI
     console.log("funziona");
     const userName = userNameElem.value;
-    const userDistanza = userDistanzElem.value;
+    const userDistanza = parseInt(userDistanzElem.value);
     const userEta = userEtaElement.value;
-    console.log(userName, userDistanza, userEta);
+    console.log(userName, userDistanza, typeof userDistanza, userEta, typeof userEta);
 
 
+    // LOGICA DEL PROGRAMMA
+    let biglettoIntero = (userDistanza * prezzoBase);
+    console.log(biglettoIntero, typeof biglettoIntero);
+
+    if ( userEta === "minore") {
+        let biglietto = biglettoIntero - (biglettoIntero * scontoMinori);
+        console.log(biglietto);
+
+
+    } else if ( userEta === "standard") {
+        let biglietto = biglettoIntero;
+        console.log(biglietto);
+
+
+    } else if ( userEta === "over") {
+        let biglietto = biglettoIntero - (biglettoIntero * scontoOver);
+        console.log(biglietto);
+    }
 
 
 
