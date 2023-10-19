f994cba4822d6c91ad851169993c0c745f45f7dc
danieldorazio <danieldorazio.97@gmail.com>
2023-10-19 14:32:30 +0200

    add: js: GLOBAL, RACCOLTA DATI

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
+/////////// GLOBAL////////////////
+
 const userNameElem = document.getElementById ("nome");
 console.log(userNameElem);
+
+const userDistanzElem = document.getElementById ("distanza");
+console.log(distanza);
+
+const userEtaElement = document.getElementById ("eta");
+console.log(userEtaElement);
+
+
+/////////////FUNZIONAMENTO AL CLICK////////////
+const sendBtn = document.getElementById("send");
+sendBtn.addEventListener("click", function () {
+
+    //RACCOLTA DATI
+    console.log("funziona");
+    const userName = userNameElem.value;
+    const userDistanza = userDistanzElem.value;
+    const userEta = userEtaElement.value;
+    console.log(userName, userDistanza, userEta);
+
+
+
+
+
+
+})
